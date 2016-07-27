1f562a6fd62dabd7e66cb0dbdfb3b58bdb1da854
Riccardo <riccardo@iMac-di-Andrea.fritz.box>
2016-07-27 11:08:45 +0200

    minor changes

--- pogom/mail.py
+++ pogom/mail.py
@@ -1,8 +1,10 @@
 import logging
 import yagmail
 from datetime import datetime
+import requests
 
-
+key = 'key-879026018cfa93cd484a3a819b218339'
+sandbox = 'sandbox2689eea951c741a79298ac186e00ebe0.mailgun.org'
 
 log = logging.getLogger(__name__)
 
@@ -22,4 +24,15 @@ def send(name, lat , long, timeString):
     yag = yagmail.SMTP('davidegiretti@gmail.com', 'heisenberg44')
 
     for email in mail_list:
-        yag.send(email, subject, text)
+        #yag.send(email, subject, text)
+
+        request_url = 'https://api.mailgun.net/v3/{0}/messages'.format(sandbox)
+        request = requests.post(request_url, auth=('api', key), data={
+            'from': 'hello@example.com',
+            'to': email,
+            'subject': subject,
+            'text': text
+        })
+
+        print 'Status: {0}'.format(request.status_code)
+        print 'Body:   {0}'.format(request.text)
